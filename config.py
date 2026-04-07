26ed9b2d950e5feb119c1a16a48a4ced84fec3c5
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2026-04-07 22:50:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = int(os.environ.get("API_ID", 22419004))
 API_HASH = os.environ.get("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8732170391:AAGalFGZ2lbkbC_wca9IFt5Z8v38fcLqH0I")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8732170391:AAHIm0_xvUCjePzRegZaK6crL_Pxdpaa9no")
 
 SET_INTERVAL = int(os.environ.get("SET_INTERVAL", 60))  # in seconds, default 1 hour
 TARGET_CHAT_ID = os.environ.get("TARGET_CHAT_ID", "-1003741006721")
@@ -17,7 +17,7 @@ MONGO_NAME = os.environ.get("MONGO_NAME", "cantarellabots")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8565045255"))
 ADMIN_URL = os.environ.get("ADMIN_URL", "@cdn_obita")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@noob_animeworld_bot")
-FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/bli70r.jpg")
+FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/3fyfxh.jpg")
 FSUB_LINK_EXPIRY = int(os.environ.get("FSUB_LINK_EXPIRY", 600))
 START_PIC =os.environ.get("START_PIC", "https://files.catbox.moe/4b8jvw.jpg")
 
@@ -36,7 +36,7 @@ PROGRESS_BAR = os.environ.get("PROGRESS_BAR", """
 # ─── Response Images ───
 # Rotating anime images sent with every bot reply. Add as many as you like.
 RESPONSE_IMAGES = [
-    "https://files.catbox.moe/5oonsm.jpg",
+    "https://files.catbox.moe/3fyfxh.jpg",
     "https://files.catbox.moe/9ufgme.jpg",
     "https://files.catbox.moe/4b8jvw.jpg",
     "https://files.catbox.moe/bli70r.jpg",
